aecd42dd836dea44c03bd13168848c7c1512d492
Fabio Burco <f.burco@gmail.com>
2016-08-29 22:13:54 +0200

    Experimenting

--- src/it/uniud/mads/docker2ldb/Docker2Ldb.java
+++ src/it/uniud/mads/docker2ldb/Docker2Ldb.java
@@ -6,12 +6,18 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Map;
 
 public class Docker2Ldb {
     public static void main(String args[]) throws FileNotFoundException {
         InputStream input = new FileInputStream(new File("./etc/docker-compose.yml"));
         Yaml yaml = new Yaml();
-        Object data = yaml.load(input);
-        System.out.println(data);
+        Map<String, Map> o = (Map<String, Map>) yaml.load(input);
+        Map<String, Map> services = o.get("services");
+        for (String service : services.keySet()) {
+            System.out.println(services.get(service).keySet());
+            System.out.println(services.get(service).values());
+        }
+        // System.out.println(services.keySet());
     }
 }
