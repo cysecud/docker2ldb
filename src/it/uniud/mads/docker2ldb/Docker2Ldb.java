a8d43148c2801276e706838d83318a0a5d638b9a
Fabio Burco <f.burco@gmail.com>
2016-08-29 21:32:52 +0200

    First examples

--- src/it/uniud/mads/docker2ldb/Docker2Ldb.java
+++ src/it/uniud/mads/docker2ldb/Docker2Ldb.java
@@ -1,7 +1,17 @@
 package it.uniud.mads.docker2ldb;
 
-/**
- * Created by fabioburco on 29/08/16.
- */
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class Docker2Ldb {
+    public static void main(String args[]) throws FileNotFoundException {
+        InputStream input = new FileInputStream(new File("./etc/docker-compose.yml"));
+        Yaml yaml = new Yaml();
+        Object data = yaml.load(input);
+        System.out.println(data);
+    }
 }
