acd79a4901f39083fdfe6325030e98678e08a5be
Fabio Burco <f.burco@gmail.com>
2016-08-30 20:58:34 +0200

    Starting to integrate with ldb class

--- src/it/uniud/mads/docker2ldb/Docker2Ldb.java
+++ src/it/uniud/mads/docker2ldb/Docker2Ldb.java
@@ -1,15 +1,25 @@
 package it.uniud.mads.docker2ldb;
 
+import it.uniud.mads.jlibbig.core.ldb.DirectedBigraph;
+import it.uniud.mads.jlibbig.core.ldb.DirectedBigraphBuilder;
+import it.uniud.mads.jlibbig.core.ldb.DirectedControl;
+import it.uniud.mads.jlibbig.core.ldb.DirectedSignature;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.LinkedList;
 import java.util.Map;
 
 public class Docker2Ldb {
     public static void main(String args[]) throws FileNotFoundException {
+        DirectedControl container = new DirectedControl("container_1", true, 1, 1);
+        DirectedControl[] controls = { container };
+        DirectedSignature signature = new DirectedSignature(controls);
+        DirectedBigraphBuilder bb = new DirectedBigraphBuilder(signature);
+        System.out.println(bb);
         InputStream input = new FileInputStream(new File("./etc/docker-compose.yml"));
         Yaml yaml = new Yaml();
         Map<String, Map> o = (Map<String, Map>) yaml.load(input);
