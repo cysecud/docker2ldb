40892ce87dcc3f9c90b56e1aaf4fe9c6f96e53ff
Fabio Burco <f.burco@gmail.com>
2016-08-30 22:20:24 +0200

    The bigraph grows

--- src/it/uniud/mads/docker2ldb/Docker2Ldb.java
+++ src/it/uniud/mads/docker2ldb/Docker2Ldb.java
@@ -1,15 +1,13 @@
 package it.uniud.mads.docker2ldb;
 
-import it.uniud.mads.jlibbig.core.ldb.DirectedBigraphBuilder;
-import it.uniud.mads.jlibbig.core.ldb.DirectedControl;
-import it.uniud.mads.jlibbig.core.ldb.DirectedSignature;
-import it.uniud.mads.jlibbig.core.ldb.Root;
+import it.uniud.mads.jlibbig.core.ldb.*;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Map;
 
 public class Docker2Ldb {
@@ -24,16 +22,26 @@ public class Docker2Ldb {
         Map<String, Map> o = (Map<String, Map>) yaml.load(input);
         Map<String, Map> services = o.get("services");
         int locality = 1;
+        OuterName net = bb.addOuterNameOuterInterface(1, "net");
         for (String service : services.keySet()) { // parse every service in docker-compose file
             System.out.println("Service: " + service);
             bb.addSite(r0); // add a site
             System.out.println("Added a site to the bigraph.");
-            bb.addInnerNameInnerInterface(locality, "net"); // add default net
+            bb.addInnerNameInnerInterface(locality, "net", net); // add default net
+            bb.addInnerNameOuterInterface(1, service, bb.addOuterNameInnerInterface(locality, service)); // expose the name
             if (services.get(service).get("expose") != null) {
                 String port = (String) services.get(service).get("expose");
                 System.out.println("Service exposes a port " + port + ", adding it to the interface.");
                 bb.addOuterNameInnerInterface(locality, port);
             }
+            if (services.get(service).get("ports") != null) {
+                List<String> mappings = (List<String>) services.get(service).get("ports");
+                for (String map : mappings) {
+                    String[] r = map.split("\\:");
+                    bb.addInnerNameOuterInterface(1, r[0], bb.addOuterNameInnerInterface(locality, r[1]));
+                }
+            }
+
             locality++;
         }
         System.out.println(bb);
